c66b51bab2dd70d5115d5e3bfd1b9328d7695723
Matheus Pinheiro <matheuscvp12@gmail.com>
2023-10-16 18:47:51 -0300

    initial commit

---
+++ src/features/SecondCounter/style.ts
@@ -0,0 +1,17 @@
+
+
+export const styles = {
+    value : {
+        fontSize: 78,
+        paddingLeft: 16,
+        paddingRight: '16px' ,
+        marginTop: '2px',
+        fontFamily: "Courier New, Courier, monospace",
+    },
+    textbox: {
+        fontSize: 32,
+        padding: '2px',
+        width: 64,
+        margin: 4,
+    }
+}
---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react"
+
+import { useAppSelector, useAppDispatch } from "../../app/hooks"
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from "./counterSlice"
+import styles from "./Counter.module.css"
+
+import { SecondCounter } from "../SecondCounter/SecondCounter"
+
+export function Counter() {
+  const count = useAppSelector(selectCount)
+  const dispatch = useAppDispatch()
+  const [incrementAmount, setIncrementAmount] = useState("2")
+
+  const [ incrementAmount2, setIncrementAmount2 ] = useState(100)
+
+  useEffect(() => {
+    setIncrementAmount2(Number(incrementAmount2 - Number(count)))
+  }, [count, incrementAmount])
+
+  const incrementValue = Number(incrementAmount) || 0
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+        <span className={styles.value}>{count}</span>
+        <SecondCounter amount={incrementAmount2}  setAmount={setIncrementAmount2}/>
+        <button
+          className={styles.button}
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+      </div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => {setIncrementAmount(e.target.value)}}
+        />
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount 2"
+          value={incrementAmount2}
+        />
+        <button
+          className={styles.button}
+          onClick={() => dispatch(incrementByAmount(incrementValue))}
+        >
+          Add Amount
+        </button>
+        <button
+          className={styles.asyncButton}
+          onClick={() => dispatch(incrementAsync(incrementValue))}
+        >
+          Add Async
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+        >
+          Add If Odd
+        </button>
+      </div>
+    </div>
+  )
+}
