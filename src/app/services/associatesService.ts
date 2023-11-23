1f3a3f45f4ad72b3343b178b3eab0da10971604c
CADU110 <cadumotaalves@gmail.com>
2023-11-23 14:35:49 -0300

    Associates

--- src/app/services/associatesService.ts
+++ src/app/services/associatesService.ts
@@ -1,65 +1,68 @@
-import { createApi} from '@reduxjs/toolkit/query/react'
-import { RootState } from '../store/store'
-import httpClient from '../api/HttpClient'
+import httpClient from "../api/HttpClient"
 
-function getAssociates() {
-  httpClient.get('/').then(response => {
-    return response.data
-  }).catch(error => {
-    console.log(error)
-  })
+async function getAssociates() {
+  return await httpClient
+    .get("/gestao/users")
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
-function getAssociate(id: number) {
-  httpClient.get(`/gestao/users/${id}`).then(response => {
-    return response.data
-  }).catch(error => {
-    console.log(error)
-    return null
-  })
+async function getAssociate(id: number) {
+  return await httpClient
+    .get(`/gestao/users/${id}`)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+      return null
+    })
 }
 
-function createAssociate(associate: any) {
-  httpClient.post('/gestao/users', associate).then(response => {
-    return response.data
-  }).catch(error => {
-    console.log(error)
-    return error
-  })
+async function postAssociate(associate: any) {
+  return await httpClient
+    .post("/gestao/users", associate)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+      return error
+    })
 }
 
-function updateAssociate(id: number, associate: any) {
-  httpClient.put(`/user/${id}`, associate).then(response => {
-    return response.data
-  }).catch(error => {
-    console.log(error)
-    return error
-  })
+async function updateAssociate(id: number, associate: any) {
+  return await httpClient
+    .put(`/user/${id}`, associate)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+      return error
+    })
 }
 
-function deleteAssociate(id: number) {
-  httpClient.delete(`/user/${id}`).then(response => {
-    return response.data
-  }).catch(error => {
-    console.log(error)
-    return error
-  })
-}
-
-function disableUser(id: number) {
-  httpClient.patch(`/user/${id}`).then(response => {
-    return response.data
-  }).catch(error => {
-    console.log(error)
-    return error
-  })
+async function deleteAssociate(id: number) {
+  return await httpClient
+    .delete(`/user/${id}`)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+      return error
+    })
 }
 
 export {
-    getAssociates,
-    getAssociate,
-    createAssociate,
-    updateAssociate,
-    deleteAssociate,
-    disableUser
+  getAssociates,
+  getAssociate,
+  postAssociate,
+  updateAssociate,
+  deleteAssociate,
 }
