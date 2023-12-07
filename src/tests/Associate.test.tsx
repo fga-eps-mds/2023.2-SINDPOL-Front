4924bfaaff95d6a36c54e2cc0993caf90f08207f
CADU110 <cadumotaalves@gmail.com>
2023-12-06 21:46:07 -0300

    more test

--- src/tests/Associate.test.tsx
+++ src/tests/Associate.test.tsx
@@ -1,23 +1,42 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent} from "@testing-library/react"
 import { Provider } from "react-redux"
 import { store } from "../app/store/store"
 import Associates from "../pages/Associates"
+import userEvent from "@testing-library/user-event"
 import { MemoryRouter } from 'react-router-dom';
 
 
 describe("Associate page", () => {
-    it('shoul render sindicalizados', () => {
-        render(
-            <Provider store={store}>
-              <MemoryRouter>
-                <Associates />
-              </MemoryRouter>
-            </Provider>
-        );
+  it('shoul render sindicalizados', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Associates />
+        </MemoryRouter>
+      </Provider>
+    );
 
-        screen.getByText('Sindicalizados');
+    screen.getByText('Sindicalizados');
+
+  });
+
+  it('shoul click in button cadastrar', () => {
+    const{ getByText  }= render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Associates />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const formbutton = getByText('Cadastrar');
+
+    userEvent.click(formbutton);
 
 
-    });
+
+  })
+
+
 
 })
