43c90cee9e9eef36079c56ca9e40d0c734277713
vital14 <limavictor49@gmail.com>
2023-12-10 23:29:11 -0300

    fix: removing /api from service

--- src/app/services/associatesService.ts
+++ src/app/services/associatesService.ts
@@ -2,7 +2,7 @@ import httpClient from "../api/HttpClient"
 
 async function getAssociates() {
   return await httpClient
-    .get("/api/gestao/users")
+    .get("/gestao/users")
     .then((response) => {
       return response.data
     })
@@ -13,7 +13,7 @@ async function getAssociates() {
 
 async function getAssociate(id: string | undefined) {
   return await httpClient
-    .get(`/api/gestao/users/${id}`)
+    .get(`/gestao/users/${id}`)
     .then((response) => {
       return response.data
     })
@@ -25,7 +25,7 @@ async function getAssociate(id: string | undefined) {
 
 async function postAssociate(associate: any) {
   return await httpClient
-    .post("/api/gestao/users", associate)
+    .post("/gestao/users", associate)
     .then((response) => {
       return response
     })
@@ -37,7 +37,7 @@ async function postAssociate(associate: any) {
 
 async function updateAssociate(id: string | undefined, associate: any) {
   return await httpClient
-    .put(`/api/gestao/users/${id}`, associate)
+    .put(`/gestao/users/${id}`, associate)
     .then((response) => {
       return response
     })
@@ -49,7 +49,7 @@ async function updateAssociate(id: string | undefined, associate: any) {
 
 async function deleteAssociate(id: string | undefined) {
   return await httpClient
-    .delete(`/api/gestao/users/${id}`)
+    .delete(`/gestao/users/${id}`)
     .then((response) => {
       return response.data
     })
@@ -61,7 +61,7 @@ async function deleteAssociate(id: string | undefined) {
 
 async function disableAssociate(id: string) {
   return await httpClient
-    .patch(`/api/gestao/users/${id}/disable`)
+    .patch(`/gestao/users/${id}/disable`)
     .then((response) => {
       return response.data
     })
@@ -73,7 +73,7 @@ async function disableAssociate(id: string) {
 
 async function enableAssociate(id: string) {
   return await httpClient
-    .patch(`/api/gestao/users/${id}/enable`)
+    .patch(`/gestao/users/${id}/enable`)
     .then((response) => {
       return response.data
     })
